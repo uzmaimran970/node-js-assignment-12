cf974cedbadb3d6c3fe98b43c294c2fd276949c3
Uzma imran <148060110+uzmaimran970@users.noreply.github.com>
2024-03-08 16:23:44 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,11 @@
+12.// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let name1: string[] = ["Miss kainat","Miss kiran","Miss farhana","Miss Dr.murk"];
+
+let message: string = "you are invited to dinner on sunday...";
+
+for (let i=0 ; i< name1.length; i++ ){
+
+    console.log(name1[i] +" "+ message );
+}
+
